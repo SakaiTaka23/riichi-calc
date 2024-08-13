727767add3fca4c5198ead9c5d471ffcde11fb6f
SakaiTaka23 <tt3.sakai@gmail.com>
2024-08-13 20:55:30 +0900

    decleard tile as struct

--- src/constants/hand.rs
+++ src/constants/hand.rs
@@ -1,39 +1,41 @@
-use crate::constants::tiles::pi::TileType;
+use crate::constants::tiles::pi::Tile;
 
 #[derive(Debug)]
-pub struct Koutsu {
-    pub tile_type: TileType,
-    pub tile_number: u8,
+pub struct KoutsuMentsu {
+    pub tile: Tile,
     pub is_open: bool,
 }
 
 #[derive(Debug)]
 pub struct Shuntsu {
-    pub start_from: TileType,
-    pub start_number: u8,
+    pub start_from: Tile,
     pub is_open: bool,
 }
 
 #[derive(Debug)]
 pub struct Kantsu {
-    pub tile_type: TileType,
-    pub tile_number: u8,
+    pub tile: Tile,
     pub is_open: bool,
 }
 
 #[derive(Debug)]
 pub struct Janto {
-    pub tile_type: TileType,
-    pub tile_number: u8,
+    pub tile: Tile,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub enum Mentsu {
-    Koutsu(Koutsu),
-    Shuntsu(Shuntsu),
-    Kantsu(Kantsu),
-    Janto(Janto),
+    /// 刻子 `bool` defines if it is open
+    Koutsu(Tile, bool),
+    /// 順子 `Tile` defines the start of the mentsu `bool` defines if it is open
+    Shuntsu(Tile, bool),
+    /// 槓子 `bool` defines if it is open
+    Kantsu(Tile, bool),
+    /// 雀頭 `bool` defines if it is open
+    Janto(Tile),
 }
+
+pub type Hand = [Mentsu; 5];
 
 pub enum WinMethod {
     Tumo,
