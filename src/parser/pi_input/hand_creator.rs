716dd1385e6c65daddbf88ce1412d855c4fe09eb
SakaiTaka23 <tt3.sakai@gmail.com>
2024-08-14 21:23:22 +0900

    implement input

--- src/parser/pi_input/hand_creator.rs
+++ src/parser/pi_input/hand_creator.rs
@@ -1,52 +1,108 @@
+use crate::constants::hand::Mentsu::{Koutsu, Shuntsu};
 use crate::constants::hand::{Hand, Mentsu};
 use crate::constants::tiles::pi::{Tile, TileType};
 use crate::parser::pi_input::PiHandColor;
 use std::collections::HashMap;
 
-fn create_hand(mut colors: PiHandColor, head: &Tile) -> Option<Hand> {
-    let mut _hands: Vec<Hand> = Vec::new();
-    // TODO 雀頭を抜き出す
-    let pi_count = match head.tile_type {
+pub fn create_hand(mut colors: PiHandColor, head: &Tile) -> Option<Hand> {
+    let head_mentsu = match head.tile_type {
         TileType::Dragon => {
             remove_two_occurrences(&mut colors.dragon, head.number);
-            colors.dragon.len()
+            create_mentu_zihai(&colors.dragon, &TileType::Dragon)
         }
         TileType::Manzu => {
             remove_two_occurrences(&mut colors.manzu, head.number);
-            colors.manzu.len()
+            create_mentu_suhai(&colors.manzu, &TileType::Manzu)
         }
         TileType::Pinzu => {
             remove_two_occurrences(&mut colors.pinzu, head.number);
-            colors.pinzu.len()
+            create_mentu_suhai(&colors.pinzu, &TileType::Pinzu)
         }
         TileType::Souzu => {
             remove_two_occurrences(&mut colors.souzu, head.number);
-            colors.souzu.len()
+            create_mentu_suhai(&colors.souzu, &TileType::Souzu)
         }
         TileType::Wind => {
             remove_two_occurrences(&mut colors.wind, head.number);
-            colors.wind.len()
+            create_mentu_zihai(&colors.wind, &TileType::Wind)
         }
     };
-    if pi_count % 3 != 0 {
+    if head_mentsu.is_none() {
+        return None;
+    }
+    let mut hands: Vec<Mentsu> = head_mentsu?;
+
+    if !colors.dragon.is_empty() && head.tile_type != TileType::Dragon {
+        let result = create_mentu_zihai(&colors.dragon, &TileType::Dragon);
+        if result.is_some() {
+            hands.append(&mut result?);
+        }
+    }
+    if !colors.manzu.is_empty() && head.tile_type != TileType::Manzu {
+        create_mentu_suhai(&colors.manzu, &TileType::Manzu);
+    }
+    if !colors.pinzu.is_empty() && head.tile_type != TileType::Pinzu {
+        create_mentu_suhai(&colors.pinzu, &TileType::Pinzu);
+    }
+    if !colors.souzu.is_empty() && head.tile_type != TileType::Souzu {
+        create_mentu_suhai(&colors.souzu, &TileType::Souzu);
+    }
+    if !colors.wind.is_empty() && head.tile_type != TileType::Wind {
+        create_mentu_zihai(&colors.wind, &TileType::Wind);
+    }
+
+    if hands.len() == 5 {
+        let result: Hand = hands.try_into().expect("unknown error");
+
+        return Some(result);
+    }
+
+    None
+}
+
+fn create_mentu_suhai(hand: &Vec<u8>, tile_type: &TileType) -> Option<Vec<Mentsu>> {
+    let anko_candidate = extract_anko(hand);
+    let anko_combinations = generate_combinations(&anko_candidate);
+
+    for ankos in anko_combinations {
+        let mut result: Vec<Mentsu> = Vec::new();
+        let mut hand = hand.clone();
+        for anko in &ankos {
+            // TODO 4枚構成に対応できないから専用の関数が必要
+            hand.retain(|&x| x != *anko);
+        }
+        for _ in 0..hand.len() % 3 {
+            let min = hand.iter().min()?;
+            if hand.contains(&(min + 1)) && hand.contains(&(min + 2)) {
+                result.push(Shuntsu(Tile { number: *min, tile_type: tile_type.clone() }, false));
+            } else {
+                continue;
+            }
+        }
+        if result.len() == hand.len() / 3 {
+            for anko in &ankos {
+                result.push(Koutsu(Tile { number: *anko, tile_type: tile_type.clone() }, false));
+            }
+            return Some(result);
+        }
+    }
+
+
+    None
+}
+
+fn create_mentu_zihai(hand: &Vec<u8>, tile_type: &TileType) -> Option<Vec<Mentsu>> {
+    let anko_candidate = extract_anko(hand);
+    if hand.len() % 3 != anko_candidate.len() {
         return None;
     }
 
-    // TODO 暗刻を抜き出す
-    // TODO それでできるか検証 暗刻なしでも検証が必要
+    let mut result: Vec<Mentsu> = Vec::new();
+    for anko in anko_candidate {
+        result.push(Mentsu::Koutsu(Tile { number: anko, tile_type: tile_type.clone() }, false));
+    }
 
-    None
-}
-
-fn create_mentu_suhai(hand: Vec<u8>) -> Option<Vec<Mentsu>> {
-    // TODO 暗刻を抜き出す
-    // TODO 全ての組み合わせでできるか検証
-
-    None
-}
-
-fn create_mentu_zihai(hand: Vec<u8>) -> Option<Vec<Mentsu>> {
-    None
+    Some(result)
 }
 
 fn remove_two_occurrences(vec: &mut Vec<u8>, target: u8) {
@@ -61,15 +117,13 @@ fn remove_two_occurrences(vec: &mut Vec<u8>, target: u8) {
     });
 }
 
-fn extract_anko(tiles: &mut Vec<u8>) -> Vec<u8> {
+fn extract_anko(tiles: &Vec<u8>) -> Vec<u8> {
     let mut counts = HashMap::new();
 
-    // タイルごとの出現回数をカウントする
     for &tile in tiles.iter() {
         *counts.entry(tile).or_insert(0) += 1;
     }
 
-    // 暗刻（3つ以上のタイル）を抽出する
     let mut anko = Vec::new();
     for (&tile, &count) in counts.iter() {
         if count >= 3 {
@@ -77,10 +131,27 @@ fn extract_anko(tiles: &mut Vec<u8>) -> Vec<u8> {
         }
     }
 
-    // 抽出した暗刻を元のベクタから削除する
+    let mut tilesc = tiles.clone();
     for &tile in anko.iter() {
-        tiles.retain(|&x| x != tile);  // 暗刻が含まれるタイルを削除
+        tilesc.retain(|&x| x != tile);
     }
 
     anko
+}
+
+fn generate_combinations(candidates: &Vec<u8>) -> Vec<Vec<u8>> {
+    let mut combinations = Vec::new();
+    combinations.push(Vec::new());
+    let n = candidates.len();
+
+    for i in 0..(1 << n) {
+        let mut combination = Vec::new();
+        for j in 0..n {
+            if i & (1 << j) != 0 {
+                combination.push(candidates[j]);
+            }
+        }
+        combinations.push(combination);
+    }
+    combinations
 }
