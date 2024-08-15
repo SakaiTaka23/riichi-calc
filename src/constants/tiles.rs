6e887a27a5f2eb4ffb2c073bf435540d6ac761d5
SakaiTaka23 <tt3.sakai@gmail.com>
2024-08-15 21:02:04 +0900

    add document on tiles

--- src/constants/tiles.rs
+++ src/constants/tiles.rs
@@ -1,14 +1,31 @@
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum TileType {
+    /// 萬子
     Manzu,
+    /// 筒子
     Pinzu,
+    /// 索子
     Souzu,
+    /// 風牌
     Wind,
+    /// 三元牌
     Dragon,
 }
 
+/// represents a single tile
+///
+/// # Fields
+/// - `number`: the number of tile [number]
+/// - `tile_type`: types in the enum [tile_type]
+///
+/// [number]: #structfield.number
+/// [tile_type]: #structfield.tile_type
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Tile {
+    /// - 1~9 for number tile types
+    /// - 1~4 for wind tile type `order`: (東, 南, 西, 北)
+    /// - 1~3 for dragon tile type `order`: (白, 發, 中)
     pub number: u8,
+    /// types in the enum [TileType]
     pub tile_type: TileType,
 }
