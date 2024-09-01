37075f74263fd023caac090fcd839d041239338b
SakaiTaka23 <tt3.sakai@gmail.com>
2024-09-01 13:15:04 +0900

    fixed copied code

--- src/finder/ii_han/chun.rs
+++ src/finder/ii_han/chun.rs
@@ -4,9 +4,9 @@ use crate::constants::status::Status;
 use crate::constants::tiles::TileType;
 use crate::finder::finder_base::YakuBase;
 
-pub struct Haku {}
+pub struct Chun {}
 
-impl YakuBase for Haku {
+impl YakuBase for Chun {
     fn validate(_: &Field, hand: &Hand, _: &Status) -> Option<(String, u8)> {
         for mentsu in hand {
             let tile = mentsu.tile();
--- src/finder/ii_han/hatu.rs
+++ src/finder/ii_han/hatu.rs
@@ -4,9 +4,9 @@ use crate::constants::status::Status;
 use crate::constants::tiles::TileType;
 use crate::finder::finder_base::YakuBase;
 
-pub struct Haku {}
+pub struct Hatu {}
 
-impl YakuBase for Haku {
+impl YakuBase for Hatu {
     fn validate(_: &Field, hand: &Hand, _: &Status) -> Option<(String, u8)> {
         for mentsu in hand {
             let tile = mentsu.tile();
