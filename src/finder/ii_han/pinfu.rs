91539ff357f5bca4088a62feb6e5cc166724a9a6
SakaiTaka23 <tt3.sakai@gmail.com>
2024-10-14 14:01:04 +0900

    add logic to check riyanmen in pinfu

--- src/finder/ii_han/pinfu.rs
+++ src/finder/ii_han/pinfu.rs
@@ -16,6 +16,8 @@ impl YakuBase for Pinfu {
                 _ => { return None }
             }
         }
+
+        if !Self::is_riyanmen(&hand) { return None; }
         Some(("平和".to_string(), 1))
     }
 }
@@ -31,6 +33,31 @@ impl Pinfu {
                 true
             }
             TileType::Dragon => { false }
+        }
+    }
+
+    fn is_riyanmen(WinningHand { hand, winning_tile }: &WinningHand) -> bool {
+        let start_number = hand.into_iter()
+            .filter(|m| match m {
+                Mentsu::Shuntsu(_, _) => true,
+                _ => false
+            })
+            .filter(|m| m.tile().tile_type == winning_tile.tile_type)
+            .into_iter()
+            .map(|m| m.tile().number)
+            .collect::<Vec<u8>>();
+
+        match winning_tile.number {
+            1 => { start_number.contains(&1) }
+            2 => { start_number.contains(&2) }
+            3 => { start_number.contains(&3) }
+            4 => { start_number.contains(&2) | start_number.contains(&4) }
+            5 => { start_number.contains(&3) | start_number.contains(&5) }
+            6 => { start_number.contains(&4) | start_number.contains(&6) }
+            7 => { start_number.contains(&5) }
+            8 => { start_number.contains(&6) }
+            9 => { start_number.contains(&7) }
+            _ => { false }
         }
     }
 }
