c3cf72e7a43a2e009fcf3760377b1e29bc0f8e50
SakaiTaka23 <tt3.sakai@gmail.com>
2024-10-20 11:06:22 +0900

    fix to not return other yaku when it is yakuman

--- src/finder/finder.rs
+++ src/finder/finder.rs
@@ -12,18 +12,29 @@ pub struct Finder {
 
 impl Finder {
     pub fn find_hand(&self) -> FoundYaku {
+        let yakuman = yakuman_yaku(&self.parsed_hand.field, &self.parsed_hand.winning_hand, &self.parsed_hand.status);
+        let is_yakuman = if yakuman.len() > 0 { true } else { false };
+
+        if is_yakuman {
+            return FoundYaku {
+                ii_han: Vec::new(),
+                ryan_han: Vec::new(),
+                san_han: Vec::new(),
+                roku_han: Vec::new(),
+                yakuman,
+            };
+        }
         let ii_han = ii_han_yaku(&self.parsed_hand.field, &self.parsed_hand.winning_hand, &self.parsed_hand.status);
         let ryan_han = ryan_han_yaku(&self.parsed_hand.field, &self.parsed_hand.winning_hand, &self.parsed_hand.status);
         let san_han = san_han_yaku(&self.parsed_hand.field, &self.parsed_hand.winning_hand, &self.parsed_hand.status);
         let roku_han = roku_han_yaku(&self.parsed_hand.field, &self.parsed_hand.winning_hand, &self.parsed_hand.status);
-        let yakuman = yakuman_yaku(&self.parsed_hand.field, &self.parsed_hand.winning_hand, &self.parsed_hand.status);
 
         FoundYaku {
             ii_han,
             ryan_han,
             san_han,
             roku_han,
-            yakuman,
+            yakuman: Vec::new(),
         }
     }
 }
