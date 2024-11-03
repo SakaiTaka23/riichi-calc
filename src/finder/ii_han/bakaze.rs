6d3509d237c5b0cb4387a99cc1fcdd8a0139a28a
SakaiTaka23 <tt3.sakai@gmail.com>
2024-11-03 13:18:13 +0900

    implement some sample of testing ii_han

--- src/finder/ii_han/bakaze.rs
+++ src/finder/ii_han/bakaze.rs
@@ -1,15 +1,17 @@
 use crate::constants::field::Field;
+use crate::constants::hand::Mentsu::Janto;
 use crate::constants::hand::WinningHand;
 use crate::constants::status::Status;
 use crate::constants::tiles::TileType;
 use crate::finder::finder_base::YakuBase;
 use crate::finder::utils::is_same_wind;
 
-pub struct Bakaze {}
+pub struct Bakaze;
 
 impl YakuBase for Bakaze {
     fn validate(field: &Field, winning_hand: &WinningHand, _: &Status) -> Option<(String, u8)> {
         for mentsu in winning_hand.hand {
+            if let Janto(_) = mentsu { continue; }
             let tile = mentsu.tile();
             if tile.tile_type != TileType::Wind { continue; }
             if is_same_wind(tile.number, &field.bakaze) {
@@ -17,5 +19,59 @@ impl YakuBase for Bakaze {
             }
         }
         None
+    }
+}
+
+#[cfg(test)]
+mod valid {
+    use crate::constants::field::Wind;
+    use crate::constants::hand::Mentsu;
+    use crate::constants::tiles::{Tile, TileType};
+    use crate::finder::finder_base::YakuBase;
+    use crate::finder::ii_han::bakaze::Bakaze;
+    use crate::finder::test_utils::{from_hand, random_field, random_janto, random_mentsu, random_status};
+    use rand::random;
+
+    #[test]
+    fn valid_bakaze() {
+        let mut field = random_field();
+        field.bakaze = Wind::East;
+        let hand = [
+            Mentsu::Koutsu(
+                Tile { tile_type: TileType::Wind, number: 1 },
+                random(),
+            ),
+            random_mentsu(true, false),
+            random_mentsu(true, false),
+            random_mentsu(true, false),
+            random_janto(false),
+        ];
+        assert_eq!(Bakaze::validate(&field, &from_hand(hand), &random_status()), Some(("役牌:場風牌".to_string(), 1)), "{:?}", hand);
+    }
+}
+
+#[cfg(test)]
+mod invalid {
+    use crate::constants::field::Wind;
+    use crate::constants::hand::Mentsu;
+    use crate::constants::tiles::{Tile, TileType};
+    use crate::finder::finder_base::YakuBase;
+    use crate::finder::ii_han::bakaze::Bakaze;
+    use crate::finder::test_utils::{from_hand, random_field, random_mentsu, random_status};
+
+    #[test]
+    fn bakaze_as_janto() {
+        let mut field = random_field();
+        field.bakaze = Wind::East;
+        let hand = [
+            Mentsu::Janto(
+                Tile { tile_type: TileType::Wind, number: 1 },
+            ),
+            random_mentsu(true, false),
+            random_mentsu(true, false),
+            random_mentsu(true, false),
+            random_mentsu(true, false),
+        ];
+        assert_eq!(Bakaze::validate(&field, &from_hand(hand), &random_status()), None, "{:?}", hand);
     }
 }
--- src/finder/ii_han/chun.rs
+++ src/finder/ii_han/chun.rs
@@ -1,19 +1,74 @@
 use crate::constants::field::Field;
-use crate::constants::hand::WinningHand;
+use crate::constants::hand::{Mentsu, WinningHand};
 use crate::constants::status::Status;
 use crate::constants::tiles::TileType;
 use crate::finder::finder_base::YakuBase;
 
-pub struct Chun {}
+pub struct Chun;
 
 impl YakuBase for Chun {
     fn validate(_: &Field, winning_hand: &WinningHand, _: &Status) -> Option<(String, u8)> {
         for mentsu in winning_hand.hand {
+            if let Mentsu::Janto(_) = mentsu { continue; }
             let tile = mentsu.tile();
             if tile.tile_type == TileType::Dragon && tile.number == 3 {
                 return Some(("役牌:中".to_string(), 1));
             }
         }
         None
+    }
+}
+
+#[cfg(test)]
+mod valid {
+    use crate::constants::hand::Mentsu;
+    use crate::constants::tiles::{Tile, TileType};
+    use crate::finder::finder_base::YakuBase;
+    use crate::finder::ii_han::chun::Chun;
+    use crate::finder::test_utils::{from_hand, random_field, random_janto, random_mentsu, random_status};
+    use rand::random;
+
+    #[test]
+    fn valid_chun() {
+        let field = random_field();
+        let hand = [
+            Mentsu::Koutsu(
+                Tile { tile_type: TileType::Dragon, number: 3 },
+                random(),
+            ),
+            random_mentsu(true, false),
+            random_mentsu(true, false),
+            random_mentsu(true, false),
+            random_janto(false),
+        ];
+        let winning_hand = from_hand(hand);
+        let status = random_status();
+        assert_eq!(Chun::validate(&field, &winning_hand, &status), Some(("役牌:中".to_string(), 1)), "{:?}", hand);
+    }
+}
+
+#[cfg(test)]
+mod invalid {
+    use crate::constants::hand::Mentsu;
+    use crate::constants::tiles::{Tile, TileType};
+    use crate::finder::finder_base::YakuBase;
+    use crate::finder::ii_han::chun::Chun;
+    use crate::finder::test_utils::{from_hand, random_field, random_mentsu, random_status};
+
+    #[test]
+    fn chun_as_janto() {
+        let field = random_field();
+        let hand = [
+            Mentsu::Janto(
+                Tile { tile_type: TileType::Dragon, number: 3 },
+            ),
+            random_mentsu(true, false),
+            random_mentsu(true, false),
+            random_mentsu(true, false),
+            random_mentsu(true, false),
+        ];
+        let winning_hand = from_hand(hand);
+        let status = random_status();
+        assert_eq!(Chun::validate(&field, &winning_hand, &status), None, "{:?}", hand);
     }
 }
