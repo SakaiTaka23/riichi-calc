c64b3c3c2ca4498e5f1401b43d686f2db8512444
SakaiTaka23 <tt3.sakai@gmail.com>
2024-11-09 11:57:57 +0900

    test roku han yaku

--- src/finder/roku_han.rs
+++ src/finder/roku_han.rs
@@ -1,15 +1,18 @@
 use crate::constants::field::Field;
 use crate::constants::hand::WinningHand;
 use crate::constants::status::Status;
-use crate::finder::finder_base::YakuBase;
+use crate::finder::finder_base::{YakuBase, YakuValidator};
 use crate::finder::roku_han::chinitu::Chinitu;
 
 mod chinitu;
 
 pub fn roku_han_yaku(field: &Field, winning_hand: &WinningHand, status: &Status) -> Vec<(String, u8)> {
-    let mut yaku = Vec::new();
+    let validators: Vec<YakuValidator> = vec![
+        Chinitu::validate,
+    ];
 
-    if let Some(y) = Chinitu::validate(field, winning_hand, status) { yaku.push(y); }
-
-    yaku
+    validators
+        .iter()
+        .filter_map(|validator| validator(field, winning_hand, status))
+        .collect()
 }
--- src/finder/roku_han/chinitu.rs
+++ src/finder/roku_han/chinitu.rs
@@ -4,7 +4,7 @@ use crate::constants::status::Status;
 use crate::finder::finder_base::YakuBase;
 use crate::finder::utils::{check_kuisagari, split_colors};
 
-pub struct Chinitu {}
+pub struct Chinitu;
 
 impl YakuBase for Chinitu {
     fn validate(_: &Field, hand: &WinningHand, _: &Status) -> Option<(String, u8)> {
@@ -20,5 +20,65 @@ impl YakuBase for Chinitu {
         }
 
         check_kuisagari(&hand.hand, "清一色".to_string(), 6)
+    }
+}
+
+#[cfg(test)]
+mod valid {
+    use crate::constants::hand::Mentsu;
+    use crate::constants::tiles::Tile;
+    use crate::finder::finder_base::YakuBase;
+    use crate::finder::roku_han::chinitu::Chinitu;
+    use crate::finder::test_utils::{from_hand, random_field, random_status, random_suhai_tile_type};
+
+    #[test]
+    fn valid_chinitu() {
+        let color = random_suhai_tile_type();
+        let hand = [
+            Mentsu::Shuntsu(Tile { tile_type: color, number: 1 }, false),
+            Mentsu::Koutsu(Tile { tile_type: color, number: 7 }, false),
+            Mentsu::Kantsu(Tile { tile_type: color, number: 2 }, false),
+            Mentsu::Koutsu(Tile { tile_type: color, number: 1 }, false),
+            Mentsu::Janto(Tile { tile_type: color, number: 9 }),
+        ];
+
+        assert_eq!(Chinitu::validate(&random_field(), &from_hand(hand), &random_status()), Some(("清一色".to_string(), 6)), "{:?}", hand);
+    }
+
+    #[test]
+    fn valid_chinitu_naki() {
+        let color = random_suhai_tile_type();
+        let hand = [
+            Mentsu::Shuntsu(Tile { tile_type: color, number: 1 }, true),
+            Mentsu::Koutsu(Tile { tile_type: color, number: 7 }, false),
+            Mentsu::Kantsu(Tile { tile_type: color, number: 2 }, false),
+            Mentsu::Koutsu(Tile { tile_type: color, number: 1 }, false),
+            Mentsu::Janto(Tile { tile_type: color, number: 9 }),
+        ];
+
+        assert_eq!(Chinitu::validate(&random_field(), &from_hand(hand), &random_status()), Some(("清一色".to_string(), 5)), "{:?}", hand);
+    }
+}
+
+#[cfg(test)]
+mod invalid {
+    use crate::constants::hand::Mentsu;
+    use crate::constants::tiles::{Tile, TileType};
+    use crate::finder::finder_base::YakuBase;
+    use crate::finder::roku_han::chinitu::Chinitu;
+    use crate::finder::test_utils::{from_hand, random_field, random_status, random_suhai_tile_type};
+
+    #[test]
+    fn valid_honitu() {
+        let color = random_suhai_tile_type();
+        let hand = [
+            Mentsu::Shuntsu(Tile { tile_type: color, number: 1 }, false),
+            Mentsu::Koutsu(Tile { tile_type: color, number: 7 }, false),
+            Mentsu::Kantsu(Tile { tile_type: color, number: 2 }, false),
+            Mentsu::Koutsu(Tile { tile_type: TileType::Wind, number: 1 }, false),
+            Mentsu::Janto(Tile { tile_type: color, number: 9 }),
+        ];
+
+        assert_eq!(Chinitu::validate(&random_field(), &from_hand(hand), &random_status()), None, "{:?}", hand);
     }
 }
