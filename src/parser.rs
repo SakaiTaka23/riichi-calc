48ba51dbbd8d81af5cde68e81c274c2242bde929
SakaiTaka23 <tt3.sakai@gmail.com>
2024-12-20 22:49:11 +0900

    format

--- src/parser.rs
+++ src/parser.rs
@@ -4,8 +4,8 @@ use crate::constants::status::Status;
 use crate::constants::tiles::Tile;
 use crate::parser::ValidationError::InvalidHand;
 
-mod pi_input;
 mod field_input;
+mod pi_input;
 mod status_input;
 
 #[derive(Debug, PartialEq)]
@@ -54,27 +54,35 @@ pub struct Input {
 }
 
 impl Input {
+    pub fn new(pi_input: PiInput, field_input: Field, status_input: Status) -> Self {
+        Self {
+            pi_input,
+            field_input,
+            status_input,
+        }
+    }
+
     pub fn parse_hand(self) -> Result<Vec<ParsedHand>, ValidationError> {
         self.validate()?;
 
         let hand_results = self.pi_input.to_mentsu();
-        if hand_results.is_none() { return Err(InvalidHand("Not a valid winning hand".to_string())); }
+        if hand_results.is_none() {
+            return Err(InvalidHand("Not a valid winning hand".to_string()));
+        }
         let (hands, red_tiles) = hand_results.unwrap();
 
         let mut result = Vec::new();
 
         for hand in hands {
-            result.push(
-                ParsedHand {
-                    field: self.field_input.clone(),
-                    winning_hand: WinningHand {
-                        hand,
-                        winning_tile: self.pi_input.hora.clone(),
-                        red_tile: red_tiles,
-                    },
-                    status: self.status_input.clone(),
-                }
-            )
+            result.push(ParsedHand {
+                field: self.field_input.clone(),
+                winning_hand: WinningHand {
+                    hand,
+                    winning_tile: self.pi_input.hora.clone(),
+                    red_tile: red_tiles,
+                },
+                status: self.status_input.clone(),
+            })
         }
 
         Ok(result)
